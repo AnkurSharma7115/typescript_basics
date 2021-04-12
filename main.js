27db2020d2f073e27401cb9bd498a2a1891b52d9
Ankur Sharma <sharma18ankur@gmail.com>
2021-04-12 11:41:40 +0530

    Varibles and its fundamentals in TypeScript

--- main.js
+++ main.js
@@ -2,3 +2,39 @@
 exports.__esModule = true;
 var message = "Hello World";
 console.log(message);
+var num1 = 10;
+var num2 = 20;
+var title = "Welcome";
+var name = "Robin"; // const variable must be initialized before use
+// Assign the type of variable
+var isValue = true;
+var total = 0;
+var first_name = "John";
+var sentence = "What is your feedback " + first_name;
+console.log(sentence);
+var n = null;
+var u = undefined;
+var isNew = null;
+var myName = undefined;
+//Array Declaration
+var list1 = [1, 2, 3];
+var list2 = [2, 3, 4];
+//limited Array declaration based on type
+var persone1 = ["Chris", 22];
+//enum Declaraion
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 5] = "Red";
+    Color[Color["Green"] = 6] = "Green";
+    Color[Color["Blue"] = 7] = "Blue";
+})(Color || (Color = {}));
+var c = Color.Green;
+console.log(c);
+//"any" type of variable and its limitations
+var randomeValue = 10;
+randomeValue = true;
+randomeValue = "Vishwas";
+var myVar = 20;
+console.log(myVar.name); //no Error - Accessing property
+myVar(); //noError - calling as a function
+myVar.toUpperCase(); //noError - calling method
--- main.ts
+++ main.ts
@@ -1,3 +1,57 @@
 export {};
 let message = "Hello World";
 console.log(message);
+
+let num1 = 10;
+let num2 = 20;
+
+let title = "Welcome";
+const name = "Robin"; // const variable must be initialized before use
+
+// Assign the type of variable
+
+let isValue: boolean = true;
+let total: number = 0;
+let first_name: string = "John";
+
+let sentence: string = `What is your feedback ${first_name}`;
+
+console.log(sentence);
+
+let n: null = null;
+let u: undefined = undefined;
+
+let isNew: boolean = null;
+let myName: undefined = undefined;
+
+//Array Declaration
+
+let list1: number[] = [1, 2, 3];
+let list2: Array<number> = [2, 3, 4];
+
+//limited Array declaration based on type
+
+let persone1: [string, number] = ["Chris", 22];
+
+//enum Declaraion
+
+enum Color {
+    Red = 5, //started value
+    Green, //6
+    Blue, //7
+}
+
+let c: Color = Color.Green;
+console.log(c);
+
+//"any" type of variable and its limitations
+
+let randomeValue: any = 10;
+randomeValue = true;
+randomeValue = "Vishwas";
+
+let myVar: any = 20;
+
+console.log(myVar.name); //no Error - Accessing property
+myVar(); //noError - calling as a function
+myVar.toUpperCase(); //noError - calling method
